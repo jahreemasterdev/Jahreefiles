b3ce9dc0e03f939befa74a0abcc5de2fb8756175
jahreemasterdev <jahreechavez022@gmail.com>
2022-04-11 13:17:10 +0800

    Initializing a vaiable and showing their value

---
+++ InitializingVariables.js
@@ -0,0 +1,15 @@
+//Initialize these three variables
+var a = 3;
+var b = 7;
+var c = "I am a ";
+
+// Do not change the code below this line
+
+a = a + 1; 
+b = b + 5;
+c = c + "string";  
+
+console.log(a);
+console.log(b);
+console.log(c);
+
